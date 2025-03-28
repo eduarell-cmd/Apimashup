5c3828f6070d0efd1241b2c44c251a7791a907d0
Daniel Beltran <danielbeltran468jb@gmail.com>
2025-03-28 11:43:13 -0600

    .

--- mashup/mashup/services.py
+++ mashup/mashup/services.py
@@ -1,7 +1,15 @@
 import requests
 
-def noticias():
+def Drivers():
+    url = "https://f1connectapi.vercel.app/api/current/drivers?limit=1"
 
-def partidos():
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return data
+    else:
+        return None
 
-def next():
+Drivers()
