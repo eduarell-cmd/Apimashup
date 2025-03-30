6358299389609ab9402a5d1391a7da9dde13cb6d
eduarell-cmd <edu.arellanes9@gmail.com>
2025-03-29 18:34:51 -0600

    .

--- mashup/mashup/services.py
+++ mashup/mashup/services.py
@@ -1,5 +1,5 @@
 import requests
-
+import http.client
 def Drivers():
     url = "https://f1connectapi.vercel.app/api/current/drivers"
 
@@ -11,3 +11,19 @@ def Drivers():
         return drivers_list
     else:
         return None
+
+def Futbol():
+    conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+    headers = {
+        'x-rapidapi-host': "v3.football.api-sports.io",
+        'x-rapidapi-key': "ef11f29081cb7e476852aa0a039679df"
+        }
+
+    conn.request("GET", "/fixtures?league=39&season=2023", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
+Futbol()
